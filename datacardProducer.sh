520b0deaddde09f5d7737cf2a3bbd7fb374087fb
Oguz Guzel <44399848+Oguz-Guzel@users.noreply.github.com>
2026-02-26 11:31:47 +0100

    merge scripts

--- datacardProducer.sh
+++ datacardProducer.sh
@@ -1,18 +1,32 @@
 #!/bin/sh
 
-# ./start_el7.sh
-
 export DATACARD_DIR=/afs/cern.ch/work/a/aguzel/private/wwbb-run3-datacards/
 
 cd BambooDatacardProducer/inference
 source setup.sh
-# source ~/.bash_profile
 105
 cd /afs/cern.ch/work/a/aguzel/private/BambooDatacardProducer
 source /afs/cern.ch/work/a/aguzel/private/wwbb-run3-datacards/datacard_env/bin/activate
 
-python produceDataCards.py --yaml $DATACARD_DIR'config/config_2022.yml'
+python produceDataCards.py --yaml $DATACARD_DIR'config/config_combined_sr.yml'
 
-python produceDataCards.py --yaml $DATACARD_DIR'config/config_2023.yml'
+python produceDataCards.py --yaml $DATACARD_DIR'config/config_combined_dy.yml'
 
-python produceDataCards.py --yaml $DATACARD_DIR'config/config_combined.yml'
+python produceDataCards.py --yaml $DATACARD_DIR'config/config_combined_tt.yml'
+
+
+
+
+
+cd /afs/cern.ch/work/a/aguzel/private/BambooDatacardProducer
+
+python merge_plotit_all_eras.py  /afs/cern.ch/work/a/aguzel/private/wwbb-run3-datacards/output/SR_v1.4.7_noRebinning_v1/plotit SR
+
+mkdir /afs/cern.ch/work/a/aguzel/private/wwbb-run3-datacards/output/SR_v1.4.7_noRebinning_v1/plotit/plots_Run3
+
+plotIt -i /afs/cern.ch/work/a/aguzel/private/wwbb-run3-datacards/output/SR_v1.4.7_noRebinning_v1/plotit -o /afs/cern.ch/work/a/aguzel/private/wwbb-run3-datacards/output/SR_v1.4.7_noRebinning_v1/plotit/plots_Run3 /afs/cern.ch/work/a/aguzel/private/wwbb-run3-datacards/output/SR_v1.4.7_noRebinning_v1/plotit/plots_Run3.yml
+
+
+
+
+# plotIt -i output/SR_2022_combined/ -o output/SR_2022_combined/plots output/SR_2022_combined/plots.yml
--- merge_plotit_all_eras.sh
+++
@@ -1,7 +0,0 @@
-cd /afs/cern.ch/work/a/aguzel/private/BambooDatacardProducer
-
-python merge_plotit_all_eras.py
-
-mkdir /afs/cern.ch/work/a/aguzel/private/wwbb-run3-datacards/output/v1.4.7/plotit/plots_Run3
-
-plotIt -i /afs/cern.ch/work/a/aguzel/private/wwbb-run3-datacards/output/v1.4.7/plotit -o /afs/cern.ch/work/a/aguzel/private/wwbb-run3-datacards/output/v1.4.7/plotit/plots_Run3 /afs/cern.ch/work/a/aguzel/private/wwbb-run3-datacards/output/v1.4.7/plotit/plots_Run3.yml
